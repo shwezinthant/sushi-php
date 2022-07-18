45fd086ff2d449978be301a71fbb94011b59e336
naypaingsoe <naypaingsoe@pop-os.localdomain>
2022-07-18 16:37:17 +0630

    change header

--- header.php
+++ header.php
@@ -106,7 +106,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Sushi</a>
+            <a class="navbar-brand" href="#">Sushi- Menu</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
                 aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
