ee6295f468acda1de0c4b5bccc24dcc0481e364b
shwezinthant <shwezinthant340@gmail.com>
2022-07-18 16:03:04 +0630

    change title

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sushi Bar</title>
+    <title>Sushi Bar- PHP</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap/css/style.css">
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
